a9bc3a1df8670268b6fbcb0a068ab57738412bfa
James McMurray <jamesmcm03@gmail.com>
2020-04-24 01:24:31 +0200

    Add some O(N) subarray solutions

--- max_subarray/src/main.rs
+++ max_subarray/src/main.rs
@@ -4,6 +4,24 @@ struct Solution {}
 
 impl Solution {
     pub fn max_sub_array(nums: Vec<i32>) -> i32 {
+        // O(N)
+        let mut cumsum: i32 = 0;
+        let mut best: i32 = std::i32::MIN;
+
+        for n in nums {
+            cumsum += n;
+            if cumsum > best {
+                best = cumsum;
+            }
+            if cumsum < 0 {
+                cumsum = 0;
+            }
+        }
+        best
+    }
+
+    pub fn max_sub_array_n2(nums: Vec<i32>) -> i32 {
+        // O(N**2)
         Solution::gen_array(&nums).1
     }
 
@@ -17,13 +35,13 @@ impl Solution {
         let mut arr = Vec::with_capacity(Solution::trinum(n));
 
         for (i, val) in nums.iter().enumerate() {
+            let tri = Solution::trinum(i);
             arr.push(*val);
             if *val > max {
                 max = *val
             }
             for j in 0..i {
-                // println!("{}, {}", i, j);
-                let newval = val + arr[j + Solution::trinum(i) - i];
+                let newval = val + arr[j + tri - i];
                 arr.push(newval);
                 if newval > max {
                     max = newval
@@ -31,6 +49,20 @@ impl Solution {
             }
         }
         (arr, max)
+    }
+
+    pub fn max_sub_array_n3(nums: Vec<i32>) -> i32 {
+        // O(N**3)
+        let mut best: i32 = std::i32::MIN;
+        for i in 0..nums.len() {
+            for j in i..nums.len() {
+                let sum = &nums[i..=j].iter().sum();
+                if *sum > best {
+                    best = *sum;
+                }
+            }
+        }
+        best
     }
 }
 
@@ -66,5 +98,31 @@ mod tests {
     #[test]
     fn test_actual2() {
         assert_eq!(Solution::max_sub_array(vec![-1]), -1);
+    }
+
+    #[test]
+    fn test_actual_n2() {
+        assert_eq!(
+            Solution::max_sub_array_n2(vec![-2, 1, -3, 4, -1, 2, 1, -5, 4]),
+            6
+        );
+    }
+
+    #[test]
+    fn test_actual2_n2() {
+        assert_eq!(Solution::max_sub_array_n2(vec![-1]), -1);
+    }
+
+    #[test]
+    fn test_actual_n3() {
+        assert_eq!(
+            Solution::max_sub_array_n3(vec![-2, 1, -3, 4, -1, 2, 1, -5, 4]),
+            6
+        );
+    }
+
+    #[test]
+    fn test_actual2_n3() {
+        assert_eq!(Solution::max_sub_array_n3(vec![-1]), -1);
     }
 }
